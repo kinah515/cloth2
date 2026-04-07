3b5939400475845cf7b4a1ca7c7ab9e76a1d5a25
Hanik Seol <hanik0515@naver.com>
2026-04-07 11:58:28 +0900

    Update mock Pinterest URLs for beach outfit

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -13,8 +13,8 @@ async def get_ootd_recommendation(request: OOTDRequest):
     
     if "바다" in query or "휴가" in query:
         recommendation_text = "바다와 잘 어울리는 화이트 리넨 셔츠에 시원한 네이비 반바지를 매치해보세요. 포인트로 라탄햇을 더하면 완벽한 휴양지 룩이 완성됩니다!"
-        pinterest_image_url = "https://i.pinimg.com/736x/mock_sea_outfit.jpg"  # 임시 Mock URL
-        pinterest_deep_link = "pinterest://search/pins/?q=mens%20beach%20outfit"
+        pinterest_image_url = "https://i.pinimg.com/736x/52/7c/18/527c18a0f88639f98c15d4ded59108d3.jpg"  # 임시 Mock URL
+        pinterest_deep_link = "https://pin.it/93SaraDyk"
     elif "데이트" in query or "소개팅" in query:
         recommendation_text = "깔끔한 인상을 주는 베이지 슬랙스와 파스텔 톤 셔츠를 추천합니다. 단정하면서도 따뜻한 느낌을 줄 수 있어요."
         pinterest_image_url = "https://i.pinimg.com/736x/mock_date_outfit.jpg" # 임시 Mock URL
